a70b5f871f72d495a218536dd8ecfed12eb7d59e
Oshani Kumarathunga <100805282+OshaniKumarathunga@users.noreply.github.com>
2025-11-04 14:04:53 +0530

    commits for Render deployment

--- notes-backend/server.js
+++ notes-backend/server.js
@@ -4,21 +4,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import notesRouter from "./routes/notes.route.js";
 import tagsRouter from "./routes/tags.route.js";
-
 dotenv.config();
-const app = express();
 
 //middleware
-app.use(cors());
+const app = express();
+app.use(cors({
+  origin: ["http://localhost:5173", "https://note-taking-osh.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
-
-// routes
-app.use("/api/notes", notesRouter);
-app.use("/api/tags", tagsRouter);
-
-app.get("/", (req, res) => {
-  res.send("Backend working 🚀");
-});
 
 // MongoDB connection
 mongoose
@@ -27,5 +21,13 @@ mongoose
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// routes
+app.use("/api/notes", notesRouter);
+app.use("/api/tags", tagsRouter);
+
+app.get("/api/health", (req, res) => res.json({ status: "ok" }));
+
+
